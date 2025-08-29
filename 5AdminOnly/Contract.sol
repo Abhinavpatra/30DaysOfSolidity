7c368d55e18bd3f9d5e3d0d5ad8b4b544a6ce774
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2025-08-29 14:11:27 +0530

    finished the Admin contract

--- 5AdminOnly/Contract.sol
+++ 5AdminOnly/Contract.sol
@@ -9,6 +9,8 @@ pragma solidity ^0.8.26;
 contract AdminOnly{
     address public  owner;
     address[] owners;
+    uint totalAmount;
+    mapping(address => uint) withdrawlAllowance;
     modifier onlyOwner(){
         require(owner == msg.sender,"Only the owner can call this function");
         _;
@@ -19,6 +21,32 @@ contract AdminOnly{
     }
 
     function transferOwnership(address _newOwner) public onlyOwner{
+        uint ownerAmount = withdrawlAllowance[owner] ;
+        withdrawlAllowance[_newOwner] = ownerAmount;
+        withdrawlAllowance[owner] = 0;
         owner = _newOwner;
     }
+
+    function addTreasure(uint256 _amount) public onlyOwner {
+        totalAmount += _amount;
+    }
+    
+    function approveWithdrawl(address _recepient, uint256 _amount) public onlyOwner{
+        require(_amount <= totalAmount, "Not enough treasure available");
+        withdrawlAllowance[_recepient] = _amount;
+    }
+
+    function withdrawTreasure(uint256 _amount) public {
+        
+        uint256 allowance = withdrawlAllowance[msg.sender];
+        require(allowance >= _amount,"You are not allowed to take this much");
+        require(_amount <= totalAmount, "Not enough money in the bank");
+        totalAmount -=_amount;
+        withdrawlAllowance[msg.sender] -= _amount;        
+    }
+
+    function getTreasureDetails() public view onlyOwner returns (uint256) {
+        return totalAmount;
+    }
 }
+

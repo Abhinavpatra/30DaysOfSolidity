c4e18563b12f539c76936bee5cdc95502cec072f
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2025-09-16 14:56:17 +0530

    added contract 7 basic functionality and modifiers

--- 7IOUContract/src/Counter.sol
+++ 7IOUContract/src/Counter.sol
@@ -2,7 +2,63 @@
 pragma solidity ^0.8.13;
 
 contract IOUContract {
+    address public owner;
+    mapping (address => uint) friendBalances;
+    mapping (address => mapping(address=>uint)) public debts;// the first one is the one who owes money to the seocnd guy.
+    // debtor -> creditor -> amount
+    address[] public friends;
+    mapping (address => bool)isAFriend;
+
+    mapping(address => uint256) public balances;
+
+    constructor(){
+        owner = msg.sender;
+        isAFriend[owner] = true;
+    } 
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"Can be called only by owner");
+        _;
+    }
+    modifier onlyRegistered(){
+        require(isAFriend[msg.sender],"only a registered Friend can call this function");
+        _;
+    }
     
+    function addFriend(address _newFriend)public onlyOwner(){
+        require(_newFriend != address(0), "Invalid address");
+        require(!isAFriend[_newFriend], "Friend already registered");
+        require(_newFriend != address(0));
+        friends.push(_newFriend);
+        isAFriend[_newFriend]= true;
+
+    }
+
+    function depositToWallet() public payable{
+        require(msg.value >= 0, "The money deposited must be more than 0.");
+        balances[msg.sender]+= msg.value;
+    }
+    
+    function recordDebt(address _debtor, uint256 _amount) public onlyRegistered() {
+        require(isAFriend[_debtor], "Everyone must be a friend");
+        debts[_debtor][msg.sender] += _amount;
+    }
+
+    function payFromWallet(address _creditor, uint256 _amount) public {
+        require(_creditor != address(0), "Invalid address");
+        require(isAFriend[_creditor], "Creditor not registered");
+        require(_amount > 0, "Amount must be greater than 0");
+        require(debts[msg.sender][_creditor] <= _amount, "");
+        debts[msg.sender][_creditor] -=_amount;
+        balances[_creditor] += _amount;
+        balances[msg.sender] -= _amount;
+    }
+    function transferEther(address payable _to, uint256 _amount) public onlyRegistered(){
+        // require(isAFriend[_to],"only friend"); modifer does this, it also by proxy takes care of _to being address(0)
+        require(balances[msg.sender] >= _amount,"Insufficient balance, deposit more if you wish to transfer that much");
+        balances[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
 }
 
 /**
